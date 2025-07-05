aab3aada52261862995394a3111708005a972db8
Taniii <mail@taniii.com>
2025-07-05 20:31:39 +0900

    Update footer with citation and two-tone design

--- front/src/components/Footer.tsx
+++ front/src/components/Footer.tsx
@@ -2,27 +2,43 @@ import { Box, Text, Link } from '@chakra-ui/react'
 
 export default function Footer() {
   return (
-    <Box as="footer" borderTopWidth="1px" mt={8} p={4} textAlign="center" fontSize="sm">
-      <Text>
-        予算書・決算書データベース (
-        <Link href="https://www.bb.mof.go.jp/hdocs/bxsselect.html" isExternal>
-          https://www.bb.mof.go.jp/hdocs/bxsselect.html
-        </Link>
-        ) をもとに,{' '}
-        <Link href="https://taniii.com" isExternal>
-          Taniii
-        </Link>{' '}
-        が作成
-      </Text>
-      <Text mt={2}>
-        ©{' '}
-        <Link href="https://taniii.com" isExternal>
-          Taniii
-        </Link>{' '}
-        <Link href="https://x.com/taniiicom" isExternal>
-          @taniiicom
-        </Link>
-      </Text>
+    <Box as="footer" mt={8} fontSize="sm">
+      <Box
+        borderTopWidth="1px"
+        p={4}
+        textAlign="center"
+        bg="gray.50"
+        _dark={{ bg: "gray.800" }}
+      >
+        <Text>
+          政府の予算書・決算書データベース (
+          <Link href="https://www.bb.mof.go.jp/hdocs/bxsselect.html" isExternal>
+            https://www.bb.mof.go.jp/hdocs/bxsselect.html
+          </Link>
+          ) をもとに,
+          <Link href="https://taniii.com" isExternal>
+            {' '}Taniii
+          </Link>
+          {' '}が作成
+        </Text>
+      </Box>
+      <Box
+        borderTopWidth="1px"
+        p={3}
+        textAlign="center"
+        bg="gray.100"
+        _dark={{ bg: "gray.700" }}
+      >
+        <Text>
+          ©{' '}
+          <Link href="https://taniii.com" isExternal>
+            Taniii
+          </Link>{' '}
+          <Link href="https://x.com/taniiicom" isExternal>
+            @taniiicom
+          </Link>
+        </Text>
+      </Box>
     </Box>
   )
 }
